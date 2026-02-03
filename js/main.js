bfdaedc61c3170f5fc8e0a02097c5f15a6769c95
qu1sty <ameserakov304@gmail.com>
2026-02-03 09:37:54 +0700

    6 lesson

--- js/main.js
+++ js/main.js
@@ -1,21 +1,36 @@
 let app = new Vue({
     el: '#app',
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
+
+    },
+
     data: {
+
         product: "Socks",
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         inStock: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
-        ]
+        ],
+        cart: 0,
+
     }
 })
